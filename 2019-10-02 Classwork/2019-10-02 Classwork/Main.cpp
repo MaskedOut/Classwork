75fbe83dad7ac0c81aa7b1b33b9a28121a1f8ffe
MaskedOut <55443481+MaskedOut@users.noreply.github.com>
2019-10-02 08:42:36 -0400

    Add files via upload

---
+++ 2019-10-02 Classwork/2019-10-02 Classwork/Main.cpp
@@ -0,0 +1,33 @@
+// Izaiah Patterson
+// Intro To CS
+// Period 1
+// Mr. Williamson
+// 2019-10-02 Classword
+
+
+#include <iostream>
+
+int main()
+{
+
+	char letterA = 'A';
+	char letterB = 'B';
+	char letterC = 'C';
+	char letterD = 'D';
+
+	int hp = 67;
+	int xp = 78;
+	int m = 8999;
+
+	bool Car = true;
+	bool Dog = false;
+
+	int number4 = '4';
+	int number6 = '6';
+	int number7 = '7';
+
+
+
+
+
+}
