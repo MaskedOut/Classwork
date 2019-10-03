6d96b374bbd1bf6fa313bf2e204b782f3185ce96
MaskedOut <55443481+MaskedOut@users.noreply.github.com>
2019-10-03 08:38:02 -0400

    Add files via upload

---
+++ 2019-10-03 Classwork/2019-10-03 Classwork/Main.cpp
@@ -0,0 +1,16 @@
+// Izaiah Patterson 
+// Intro To CS
+// 2019-10-03
+// Mr. Williams
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "number?";
+		int x = 7;
+		cin >> x;
+		cout << "cool" << x << "is my favorite number too" << std::endl;
+
+}
